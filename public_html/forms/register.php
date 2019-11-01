1dd29e43c88c4208222c7ed51eb7da5831ff7e37
Saul Flores <saulpolo95@gmail.com>
2019-10-31 19:05:15 -0700

    Working register form, need to add random text generator

--- public_html/forms/register.php
+++ public_html/forms/register.php
@@ -10,22 +10,22 @@
 	if(!$conn) {
 		die("Could not connect: ".mysqli_connect_error());
 	} else {
-		$email ="tests";  //test_input($_POST['email']);
-		$name = "tests"; //test_input($_POST['name']);
-		$phone =  "tests"; //test_input($_POST['phone']);
-		$address = "tests"; // test_input($_POST['address']);
-		$city = "tests"; // test_input($_POST['city']);
-		$country = "tests"; // test_input($_POST['country']);
-		$occupation ="tests";   // test_input($_POST['occupation']);
-		$profession = "tests"; // test_input($_POST['profession']);
-		$reason = "tests"; // test_input($_POST['reason']);
-		$relation = "tests"; //  test_input($_POST['relation']);
-		$found_out =  "tests"; // test_input($_POST['found_out']);
-		$institution =  "tests"; // test_input($_POST['institution']);
-		$institution_address = "tests"; // test_input($_POST['institution_address']);
-		$constancy =  "tests"; //test_input($_POST['constancy']);
-		//$sql="INSERT INTO registro-usuarios-2019(email, date, name, phone, address, city, country, occupation, profession, reason, relation, found_out, institution, institution_address, constancy)VALUES('$email','$name','$phone','$address','$city','$country','$occupation','$profession','$reason','$relation','$found_out','$institution','$institution_address')";
-        $sql = "INSERT INTO `registro-usuarios-2019` (`id`, `email`, `date`, `name`, `phone`, `address`, `city`, `country`, `occupation`, `profession`, `reason`, `relation`, `found_out`, `institution`, `institution_address`, `constancy`) VALUES (NULL, 'test@gmail.com', CURRENT_TIMESTAMP, 'sadfds', 'fsdfadsf', 'sdfdasf', 'safdasfads', 'asdfdaf', 'safadsf', 'sadfsd', 'asf', 'asf', 'asdfsda', 'sdfsadf', 'sadfads', 'asdfafd');";
+		$email = test_input($_POST['email']);
+		$name = test_input($_POST['name']);
+		$phone = test_input($_POST['phone']);
+		$address = test_input($_POST['address']);
+		$city = test_input($_POST['city']);
+		$country = test_input($_POST['country']);
+		$occupation = test_input($_POST['occupation']);
+		$profession = test_input($_POST['profession']);
+		$reason = test_input($_POST['reason']);
+		$relation = test_input($_POST['relation']);
+		$found_out = test_input($_POST['found_out']);
+		$institution = test_input($_POST['institution']);
+		$institution_address = test_input($_POST['institution_address']);
+		$constancy =  test_input($_POST['constancy']);
+        $sql = "INSERT INTO `registro-usuarios-2019` (`id`, `email`, `date`, `name`, `phone`, `address`, `city`, `country`, `occupation`, `profession`, `reason`, `relation`, `found_out`, `institution`, `institution_address`, `constancy`) VALUES (NULL, '$email', CURRENT_TIMESTAMP, '$name', '$phone', '$address', '$city', '$country', '$occupation', '$profession', '$reason', '$relation', '$found_out', '$institution', '$institution_address', '$constancy');";
+
         if(mysqli_query($conn,$sql)) {
 			echo "Registro exitoso!";
 		} else {
